f45a4222971dafe0cebba0643ea1d68e534cc7b6
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2023-12-28 00:06:23 +0530

    update file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try{
+     const existingContent= await fs.readFile(fileName,{encoding:'utf8'});
+
+     const updatedContent=`${existingContent.trim(),fileContent}`;
+
+     const updateFile=await fs.writeFile(fileName.updatedContent);
+
+     return updateFile;
+  }
+  catch(e){
+    console.error(e);
+  }
   
 };
 
